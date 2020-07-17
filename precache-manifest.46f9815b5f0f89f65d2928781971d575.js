a865d015794fa2123c56c4619f6b77a5e1235ace
RAJESH <gole8701@gmail.com>
2020-07-18 01:39:52 +0530

    Updates

---
+++ precache-manifest.46f9815b5f0f89f65d2928781971d575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773b535dce5ec1bda7b6ca22d5dd879c",
+    "url": "/queuelive/index.html"
+  },
+  {
+    "revision": "561f9b64d0e4d372dd31",
+    "url": "/queuelive/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "f33e8a6cc001bb219668",
+    "url": "/queuelive/static/css/main.2454e802.chunk.css"
+  },
+  {
+    "revision": "561f9b64d0e4d372dd31",
+    "url": "/queuelive/static/js/2.69202f13.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/queuelive/static/js/2.69202f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33e8a6cc001bb219668",
+    "url": "/queuelive/static/js/main.73b344e2.chunk.js"
+  },
+  {
+    "revision": "6eb468dd78ffa2a8cc39",
+    "url": "/queuelive/static/js/runtime-main.77d7f4cc.js"
+  }
+]);
